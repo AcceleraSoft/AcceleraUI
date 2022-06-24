12a664c04e93062e9845c51333072520fea696d4
Sam Vervaeck <samvv@pm.me>
2022-06-24 16:00:35 +0200

    Update <IntegerField />

--- gearsui/src/IntegerField.tsx
+++ gearsui/src/IntegerField.tsx
@@ -1,4 +1,6 @@
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useId } from "react";
 import { Theme } from "./theme";
 
 const inputCss = (theme: Theme) => css`
@@ -10,26 +12,49 @@ border-radius: ${theme.borderRadius * 0.5}rem;
 border: 1px solid ${theme.colors.default.bg00};
 `
 
+export interface ChangeEvent {
+  value: number;
+}
+
 export interface IntegerFieldProps {
+  label?: React.ReactNode;
   value?: number;
   min?: number;
   max?: number;
   step?: number;
+  inline?: boolean;
   onChange?: (e: ChangeEvent) => void;
 }
 
+const inlineCss = css`
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+`
+
+const Label = styled.label`
+font-weight: bold;
+padding: 0.5rem;
+`
+
 export const IntegerField: React.FC<IntegerFieldProps> = ({
+  inline,
+  label,
   onChange,
   ...props
 }) => {
+  const id = useId();
   const onInput = (e: React.FormEvent<HTMLInputElement>) => {
     if (onChange !== undefined) {
       onChange({
-        value: e.currentTarget.value
-      })
+        value: parseInt(e.currentTarget.value)
+      });
     }
   }
   return (
-    <input css={inputCss} type="number" {...props} onInput={onInput} />
+    <div css={inline ? inlineCss : []}>
+      {label && <Label htmlFor={id}>{label}</Label>}
+      <input id={id} css={inputCss} type="number" {...props} onInput={onInput} />
+    </div>
   )
 }
